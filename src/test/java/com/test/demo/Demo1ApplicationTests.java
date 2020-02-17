77c4a24270ddb23ea7057b867097e5fc4bac0144
chchen <hillwhite67@gmail.com>
2020-02-17 22:16:20 +0800

    增加了JUnit测试功能

---
+++ src/test/java/com/test/demo/Demo1ApplicationTests.java
@@ -0,0 +1,19 @@
+package com.test.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.print("Hello World! /n");
+	}
+
+}
